153896e60ded742efd19771defc54c05d28b3b80
Melanie Berrut <melanieberrut.dev@gmail.com>
2018-07-01 09:49:56 +0100

    Global Element - enabled prettier

--- src/components/HeaderWishlistLink/index.js
+++ src/components/HeaderWishlistLink/index.js
@@ -7,8 +7,13 @@ class HeaderWishlistLink extends Component {
   render() {
     return (
       <NavLink to="/wishlist" className="wishlist-btn btn btn-link btn-link--header ml-3">
-        <img src={iconWishlist} className="wishlist-btn__icon icon icon--header" alt="wishlist" aria-label="wishlist" /> 
-        <span className="sr-only">Wishlist</span> 
+        <img
+          src={iconWishlist}
+          className="wishlist-btn__icon icon icon--header"
+          alt="wishlist"
+          aria-label="wishlist"
+        />
+        <span className="sr-only">Wishlist</span>
         <WishlistWidget />
       </NavLink>
     );
