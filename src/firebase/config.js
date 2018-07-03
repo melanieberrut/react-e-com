cc3465d957e977e753db290e6a1bf3202c93792e
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2018-07-04 01:05:48 +0200

    firbase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,3 @@
-var config = {
-  apiKey: 'AIzaSyAUiNtGXE6S4OQUBnFzphhBvjWZd7XCm4g',
-  authDomain: 'react-com-test.firebaseapp.com',
-  databaseURL: 'https://react-com-test.firebaseio.com',
-  projectId: 'react-com-test',
-  storageBucket: 'react-com-test.appspot.com',
-  messagingSenderId: '930668074191'
-};
+var config = {};
 
 export default config;
